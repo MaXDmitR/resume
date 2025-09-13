8f2de2d1d30695585048ccc2aceb016cbbc8b579
MaXDmitR <maxdmitrenko111@gmail.com>
2025-09-13 10:00:01 +0300

    add adaptivity

--- src/components/heading/navbar.jsx
+++ src/components/heading/navbar.jsx
@@ -2,6 +2,7 @@ import Logo from "./logo";
 import NavButton from "./nav_button";
 import React, { useState, useEffect } from 'react';
 import { HiArrowCircleUp } from "react-icons/hi";
+import { FaBars } from "react-icons/fa";
 import scrollToTop from '../../scripts/scrollToTop';
 
 
@@ -31,8 +32,8 @@ const Navbar = (props) => {
             <div className="container-fluid">
                 <Logo />
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <button className="navbar-toggler toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <FaBars style={{ color: "grey", fontSize: "28px" }} />
                 </button>
 
                 <div className="collapse navbar-collapse " id="navbarNav">
